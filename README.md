01be69767fbd8b17a83e4a6651d88f992a6b0b8f
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-10-04 13:49:18 +0300

    add `user` query route and improve comments

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Axum REST API Template
 
+Simple example for using Axum framework for a REST API.
+
 <https://github.com/tokio-rs/axum>
 
 ## Running locally
@@ -14,6 +16,9 @@ Test routes:
 
 ```shell
 curl -s http://127.0.0.1:3000 | jq .
+
+curl -s http://127.0.0.1:3000/user?username=akseli | jq .
+curl -s http://127.0.0.1:3000/user?username=pizzalover9000 | jq .
 
 curl -s -H "Content-Type: application/json" -d '{"username":"test"}' http://127.0.0.1:3000/users | jq .
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,34 @@
 //! Run with
 //!
 //! ```not_rust
-//! cargo run -p example-readme
+//! cargo run
 //! ```
 
 use axum::{
+    extract::Query,
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
     Json, Router,
 };
-use chrono::{DateTime, Utc};
+use axum_macros::debug_handler;
+use chrono::Utc;
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 
 // basic handler that responds with a static string
+#[debug_handler]
+/// Root returns a simple json response with the current date and time
 async fn root() -> impl IntoResponse {
-    let response = Response {
-        message: chrono::Utc::now().to_rfc2822(),
-    };
-    (StatusCode::CREATED, Json(response))
+    let datetime = Utc::now().to_rfc2822();
+    tracing::info!("Root: {}", datetime);
+    (StatusCode::OK, Json(Response { message: datetime }))
 }
 
+// debug handler macro generates better error messages in Rust compile
+// https://docs.rs/axum-macros/latest/axum_macros/attr.debug_handler.html
+#[debug_handler]
+/// Example for doing a POST with some data
 async fn create_user(
     // this argument tells axum to parse the request body
     // as JSON into a `CreateUser` type
@@ -33,16 +40,34 @@ async fn create_user(
         username: payload.username,
     };
 
-    tracing::info!("User created: {}", user.username);
+    tracing::info!("Create user: {}", user.username);
 
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
+    // This will be converted into a JSON response with a status code of `201 Created`.
     (StatusCode::CREATED, Json(user))
+}
+
+#[debug_handler]
+/// Example for using query parameters
+async fn query_user(Query(user): Query<UserQuery>) -> impl IntoResponse {
+    tracing::info!("Query user: {}", user.username);
+
+    (
+        StatusCode::OK,
+        Json(User {
+            id: 1234,
+            username: user.username,
+        }),
+    )
 }
 
 #[derive(Deserialize)]
 struct CreateUser {
     username: String,
+}
+
+#[derive(Deserialize)]
+pub struct UserQuery {
+    pub username: String,
 }
 
 #[derive(Serialize)]
@@ -58,17 +83,19 @@ struct Response {
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
+    // Initialize tracing
     tracing_subscriber::fmt::init();
 
-    // build application with a route
+    // Build application with routes
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root))
+        // `GET /user` goes to `query_user`
+        .route("/user", get(query_user))
         // `POST /users` goes to `create_user`
         .route("/users", post(create_user));
 
-    // run app with hyper
+    // Run app with Hyper
     // `axum::Server` is a re-export of `hyper::Server`
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("listening on {}", addr);
