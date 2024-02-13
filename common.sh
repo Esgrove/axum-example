f56ee40301143f7d51c2ae659a137ad23bada7c5
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2024-02-13 22:36:18 +0200

    use more colors in test script

---
+++ common.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+set -eo pipefail
+
+# Get absolute path to repo root
+REPO_ROOT=$(git rev-parse --show-toplevel || (cd "$(dirname "${BASH_SOURCE[0]}")" && pwd))
+export REPO_ROOT
+
+# Check platform:
+# BSD (Mac) and GNU (Linux & Git for Windows) coreutils implementations
+# have some differences for example in the available command line options.
+case "$(uname -s)" in
+    "Darwin")
+        export BASH_PLATFORM="mac"
+        ;;
+    "MINGW"*)
+        export BASH_PLATFORM="windows"
+        ;;
+    *)
+        export BASH_PLATFORM="linux"
+        ;;
+esac
+export BASH_PLATFORM
+
+green() {
+    printf "\e[1;49;32m%s\e[0m" "$1"
+}
+
+# Print a message with green color
+print_green() {
+    printf "\e[1;49;32m%s\e[0m\n" "$1"
+}
+
+# Print a message with magenta color
+print_magenta() {
+    printf "\e[1;49;35m%s\e[0m\n" "$1"
+}
+
+red() {
+    printf "\e[1;49;31m%s\e[0m" "$1"
+}
+
+# Print a message with red color
+print_red() {
+    printf "\e[1;49;31m%s\e[0m\n" "$1"
+}
+
+# Print a message with yellow color
+print_yellow() {
+    printf "\e[1;49;33m%s\e[0m\n" "$1"
+}
+
+# Print an error and exit the program
+print_error_and_exit() {
+    print_red "ERROR: $1"
+    # use exit code if given as second argument, otherwise default to 1
+    exit "${2:-1}"
+}
+
+# Print usage and exit the program. An optional error message can be given as well.
+print_usage_and_exit() {
+    if [ $# -eq 1 ]; then
+        print_red "ERROR: $1"
+    fi
+    if [ -z "$USAGE" ]; then
+        print_red "No usage text provided in variable USAGE"
+    else
+        echo "$USAGE"
+    fi
+    # use exit code if given as second argument, otherwise default to 1
+    exit "${2:-1}"
+}
+
+# Get the Rust executable name from Cargo.toml
+get_rust_executable_name() {
+    local executable
+    executable=$(awk -F'=' '/\[\[bin\]\]/,/name/ {if($1 ~ /name/) print $2}' Cargo.toml | tr -d ' "')
+    # If no name under [[bin]], get the package name
+    if [ -z "$executable" ]; then
+        executable=$(awk -F'=' '/\[package\]/,/name/ {if($1 ~ /name/) print $2}' Cargo.toml | tr -d ' "')
+    fi
+    if [ "$BASH_PLATFORM" = windows ]; then
+        executable="${executable}.exe"
+    fi
+    echo "$executable"
+}
--- test-routes.sh
+++ test-routes.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 set -eo pipefail
 
+# Import common functions
+DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+# shellcheck source=../common.sh
+source "$DIR/common.sh"
+
 get() {
     local url="$1"
-    echo "GET: $1"
+    print_magenta "GET: $1"
     response=$(curl -s -w "%{http_code}" -o response.json "$url")
-    echo "Status code: $response"
-    jq < response.json
-    rm response.json
+    print_response "$response"
 }
 
 post() {
     local url="$1"
     local data="$2"
-    echo "POST: $1 $2"
+    print_magenta "POST: $1 $2"
     response=$(curl -s -H "Content-Type: application/json" -d "$data" -w "%{http_code}" -o response.json "$url")
-    echo "Status code: $response"
+    print_response "$response"
+}
+
+print_response() {
+    local response="$1"
+    if echo "$response" | grep -q '^2'; then
+        echo "Status code: $(green "$response")"
+    elif echo "$response" | grep -q '^4'; then
+        echo "Status code: $(red "$response")"
+    else
+        echo "Status code: $response"
+    fi
     jq < response.json
     rm response.json
 }
 
 if ! curl -s -o /dev/null -w "%{http_code}" "http://127.0.0.1:3000" | grep -q '^2'; then
-    echo "Failed to call API, is it running?"
-    exit 1
+    print_error_and_exit "Failed to call API, is it running?"
 fi
 
 get http://127.0.0.1:3000
